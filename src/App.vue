48f85ed66093f376e6dcf781501e7b494c6e1bb9
rojap0 <240769@edu.p.lodz.pl>
2026-07-24 22:01:08 +0200

    Stworzenie projektu

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="background">
+    <div class="main-page">
+      <a
+        href="https://www.instagram.com/corner.eng/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="ig-logo"
+      >
+        <i class="fab fa-instagram"></i>
+      </a>
+
+      <div class="brand-container">
+        <h1 class="brand-title">corner</h1>
+        <h2 class="brand-title">.eng</h2>
+        <p class="brand-author">by martyna narożnik</p>
+      </div>
+    </div>
+  </div>
+</template>
