28c99f700a44d7bfef420dfa47a06e5aee0cb619
Rojap02 <szymon.pajor02@gmail.com>
2026-09-01 01:27:34 +0200

    Dodanie widoku zapisów, poprawienie widoku ofert i zmiany kosmetyczne

--- src/views/SignUpPage.vue
+++ src/views/SignUpPage.vue
@@ -1,7 +1,24 @@
-<scrip setup lang="ts"></scrip>
+<script setup lang="ts">
+  import "@/assets/styles/sign-up-page-style.css";
+</script>
 
 <template>
-  <main>
-    <h1>Artykuly</h1>
-  </main>
+  <section class="sign-up">
+    <div class="sign-up-header">
+      <h2>rozpocznij współpracę</h2>
+    </div>
+
+    <div class="sign-up-text">
+      <p>przed zapisaniem się na zajęcia, zachęcam do zapoznania się z <RouterLink to="/regulamin">regulaminem.</RouterLink><br>jeżeli podczas wypełniania formularza pojawią się pytania, bądź niejasności, proszę o kontakt mailowy</p>
+    </div>
+
+    <div class="sign-up-button">
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLScd5s0jLeI3wtgeIPHis6SmEp9jcewBHnponTCLkRA0vw3hJA/closedform?pli=1" 
+      class="btn"
+      target="_blank" 
+      rel="noopener noreferrer"
+      aria-label="Formularz zapisów na zajęcia językowe w corner.eng"
+      >WYPEŁNIJ FORMULARZ ZGŁOSZENIOWY</a>
+    </div>
+  </section>
 </template>
