d9609beb847147c42ea6cf3aa414c8fe9660f88b
Rojap02 <szymon.pajor02@gmail.com>
2026-09-01 17:05:01 +0200

    Dodanie widoku 'O mnie' oraz pliku ze stylem dla tego widoku + poprawki wynikające z dodania widoku

--- src/views/AboutMePage.vue
+++ src/views/AboutMePage.vue
@@ -1,7 +1,63 @@
-<scrip setup lang="ts"></scrip>
+<script setup lang="ts">
+import "@/assets/styles/about-me-page-style.css";
+</script>
 
 <template>
-  <main>
-    <h1>o mnie</h1>
-  </main>
+  <section class="hello">
+    <div class="hello-header">
+      <h2>hello, tu martyna!</h2>
+    </div>
+
+    <div class="hello-body">
+      <p>Mam 23 lata i od kiedy pamiętam, po prostu lubię angielski - jego brzmienie, klimat i to, jaką wolność daje w komunikacji.
+        Zawsze świetnie sobie z nim radziłam, nawet zaczęłam studia w tym kierunku, 
+        jednak po czasie zdecydowałam się zmienić kierunek na psychologię.
+        Aktualnie jestem na IV roku i już za moment zaczynam pierwsze praktyki.<br><br><br>
+        Dlaczego nie kontynuowałam filologii? Denerwowała mnie jej podręcznikowa surowość. 
+        Klucz odpowiedzi często wymagał powiedzenia czegoś w jeden konkretny sposób,
+        podczas gdy w prawdziwym życiu można to ująć na 30 innych sposobów… Dlatego też, 
+        zależy mi na tym, by gramatyka była dla moich uczniów przydatnym narzędziem, 
+        a nie powodem do stresu czy paniki.<br><br><br>
+        Połączenie psychologii z nauczaniem daje mi zupełnie inne spojrzenie na lekcje. 
+        Świetnie rozumiem blokady, stres i te momenty,kiedy słowa giną nam 
+        na przysłowiowym „końcu języka”. Choć na co dzień jestem raczej introwertyczką 
+        ceniącą sobie spokój, praca z ludźmi i nauczanie stały się dla mnie ogromną 
+        frajdą i przychodzą mi z dużą lekkością.</p>
+    </div>
+  </section>
+
+  <section class="lessons">
+    <div class="lessons-header">
+      <h2>jak wygląda <br>moje podejście do zajęć?</h2>
+    </div>
+
+    <div class="lessons-body">
+      <p>
+        Pracuję z osobami na bardzo różnych etapach - od uczniów przygotowujących się do egzaminu ósmoklasisty czy matury,
+        przez licealistów i studentów, aż po dorosłych, którzy chcą przełamać barierę w mówieniu lub po prostu polubić angielski na nowo.<br><br><br>
+        Nie prowadzę zajęć według jednego, sztywnego schematu. Korzystam z podręczników, ale traktuję je jedynie jako bazę. Łączę je z własnymi notatkami, prezentacjami i materiałami dopasowanymi do konkretnej osoby. Z każdym moje lekcje wyglądają inaczej, bo każdy uczy się w swoim tempie: czasami przerabiamy masę materiałów na jednej lekcji angielskiego, a innym razem zadam jedno trafne pytanie i prowadzimy o nim dyskusję przez pełne 60 minut. Swobodna rozmowa, przełamywanie bariery i Twój komfort psychiczny są dla mnie o wiele ważniejsze niż bezduszne przerabianie kolejnych stron  książki czy kolejna powtórka tego samego czasu gramatycznego.
+      </p>
+    </div>
+  </section>
+
+  <section class="private">
+    <div class="private-header">
+      <h2>a prywatnie?</h2>
+    </div>
+
+    <div class="private-body">
+      <p>Jestem raczej spokojną introwertyczką i po prostu doceniam święty spokój. 
+        W wolnym czasie od lat maluję farbami olejnymi, mam w domu sporo roślin
+        i lubię zajęcia kreatywne - chociażby z dnia na dzień wymyślić sobie lepienie z gliny albo rozłożyć na podłodze puzzle
+        na 1000 elementów (które potem leżą tam przez parę tygodni, dopóki ich nie dokończę).
+        Uwielbiam gotować i piec, chociaż kuchnia po moim „gotowaniu twórczym” często wygląda jak pobojowisko.
+        Sama też nieustannie doszkalam swój angielski — bo nie da się znać wszystkich słówek na świecie!
+        Tak samo jak moi uczniowie, regularnie korzystam z Quizleta. A w wolnych chwilach uczę się hiszpańskiego,
+        więc doskonale pamiętam, jak to jest być po drugiej stronie i krok po kroku budować nawyk nauki.<br><br><br>
+        Aha, prowadzę też profil <RouterLink class="private-instagram" to="/corner.ang">@corner.ang</RouterLink>.
+        Jednak daleko mi od bycia influencerką… <br>Wrzucam tam po prostu sprytniejsze podejście do angielskiego, ciekawostki psychologiczne i pokazuję, jak to wygląda od kulis.
+      </p>
+    </div>
+  </section>
+
 </template>
